1a3eace422b37836c4ad2c7b54cd714c57ccae31
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2019-03-31 17:11:08 +0900

    Add create_tone_list func

--- py_rap_gen/utils.py
+++ py_rap_gen/utils.py
@@ -16,10 +16,17 @@ tone_types["u"] = ['ウ', 'ク', 'ス', 'ツ', 'ヌ', 'フ', 'ム', 'ユ', 'ル'
 tone_types["e"] = ['エ', 'ケ', 'セ', 'テ', 'ネ', 'ヘ', 'メ', 'レ', 'ゲ', 'ゼ', 'デ', 'ベ', 'ペ', 'ェ']
 tone_types["o"] = ['オ', 'コ', 'ソ', 'ト', 'ノ', 'ホ', 'モ', 'ヨ', 'ロ', 'ヲ', 'ゴ', 'ゾ', 'ド', 'ボ', 'ポ', 'ョ', 'ォ']
 tone_types["xtu"] = ['ッ']
+tone_types["n"] = ['ン']
 mini = ['ァ', 'ィ', 'ゥ', 'ェ', 'ォ', 'ャ', 'ュ', 'ョ']
 
 
 def _convert_tones(kana):
+    """Convert katakana to tone.
+    Args:
+        kana (List[String]): kana list.
+    Return:
+        tones (List[String]): tone list.
+    """
     tones = []
     for k in kana:
         if k in mini and len(tones) != 0:
@@ -29,7 +36,32 @@ def _convert_tones(kana):
                 tones.append(t)
     return tones
 
-for k in counter['children']:
-    a = mecab.parse(k).strip().split()[0]
-    print(a)
-    print(_convert_tones(a))
+
+def _create_tone_list(counter):
+    """Return tone to string dictionary.
+
+    Return:
+        tone_list (Hash[String, List[String]]): tone to string dictionary.
+    """
+    tone_list = {}
+    for k in counter['children']:
+        chars = mecab.parse(k).strip().split()[0]
+        tones = "".join(_convert_tones(chars))
+        if tones != "":
+            if tones not in tone_list:
+                tone_list[tones] = []
+            tone_list[tones].append(chars)
+
+            for k1 in counter['children'][k]['children']:
+                chars1 = mecab.parse(k1).strip().split()[0]
+                tones1 = "".join(_convert_tones(chars1))
+                if tones1 != "":
+                    tones_double = tones + tones1
+                    if tones_double not in tone_list:
+                        tone_list[tones_double] = []
+                    tone_list[tones_double].append(chars + chars1)
+    return tone_list
+
+
+# with open('mecab_tone_2gram.pkl', 'wb') as w:
+#     pickle.dump(_create_tone_list(counter), w, pickle.HIGHEST_PROTOCOL)
--- tests/test_py_rap_gen.py
+++ tests/test_py_rap_gen.py
@@ -12,3 +12,40 @@ def test_convert_tones():
     eq_(['a', 'i', 'o', 'u', 'i', 'a'], utils._convert_tones('カリフォルニア'))
     eq_(['o', 'u', 'e', 'u'], utils._convert_tones('チョウセツ'))
     eq_(['o', 'a', 'e', 'i', 'o', 'u'], utils._convert_tones('ローマテイコク'))
+    eq_([], utils._convert_tones('、'))
+    eq_(['a', 'n', 'a', 'n'], utils._convert_tones('カンタン'))
+
+def test_create_tone_list():
+    counter = {
+        'children': {
+            '頭': {
+                'children': {
+                    'が': {
+
+                    },
+                    'は': {
+
+                    }
+                }
+            },
+            '動き': {
+                'children': {
+                    'は': {
+
+                    },
+                    '出す': {
+                        
+                    }
+                }
+            }
+        }
+    }
+    ret = utils._create_tone_list(counter)
+    expected = {
+        "aaa": ['アタマ'],
+        "aaaa": ['アタマガ', 'アタマハ'],
+        "uoi": ['ウゴキ'],
+        "uoia": ['ウゴキハ'],
+        "uoiau": ['ウゴキダス']
+    }
+    eq_(expected, ret)
