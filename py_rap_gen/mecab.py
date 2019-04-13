b9c410f4dc75f0d8982dd4ffe1f24688b8791677
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2019-04-13 21:28:49 +0900

    Add mecab module

---
+++ py_rap_gen/mecab.py
@@ -0,0 +1,71 @@
+# Limitations under the MIT License.
+# Copyright 2019 Katsuya Shimabukuro.
+
+import MeCab
+
+tagger = MeCab.Tagger("")
+
+
+class Word(object):
+    """Word class from mecab output."""
+
+    def __init__(self, output):
+        """Initialize mecab one word class from mecab one line."""
+        pos, pos1, surface, base, yomi = self._parse_mecab_output(output)
+        self.pos = pos
+        self.pos1 = pos1
+        self.surface = surface
+        self.base = base
+        self.yomi = yomi
+
+    @classmethod
+    def _parse_mecab_output(cls, output):
+        """Return mecab one word parsing results.
+
+        Args:
+            output (str): mecab one line.
+        Return:
+            pos (str): most import part-of-speech tag.
+            pos1 (str): second import part-of-speech tag.
+            surface (str): word surface format string.
+            base (str): word base format string.
+            yomi (str): word surface yomi string.
+        """
+        parts = output.split("\t")[1].strip().split(",")
+        pos = parts[0]
+        pos1 = parts[1]
+        base = parts[6]
+        yomi = parts[7]
+        surface = output.split("\t")[0].strip()
+        return pos, pos1, surface, base, yomi
+
+
+class Sentence(object):
+    """Sentence class from mecab output."""
+
+    def __init__(self, output):
+        """Initialize mecab one Sentence class from mecab one line."""
+        self.words = self._parse_mecab_output(output)
+
+    @classmethod
+    def _parse_mecab_output(cls, output):
+        """Return mecab sentence parsing results.
+
+        Args:
+            output (str): mecab outputs.
+        Return:
+            words (List[Word]): parsing output word class array.
+        """
+        words = [Word(line) for line in output.strip().split('\n') if line != 'EOS']
+        return words
+
+
+def parse(s):
+    """Parsing mecab.
+
+    Args:
+        s (str): target string.
+    Return:
+        sentence (Sentence): parsed sentence object.
+    """
+    return Sentence(tagger.parse(s))
---
+++ tests/test_mecab.py
@@ -0,0 +1,32 @@
+from nose.tools import ok_, eq_
+from py_rap_gen import mecab
+
+
+def test_mecab_case1():
+    sentence = mecab.parse("青い空")
+    eq_(sentence.words[0].surface, "青い")
+    eq_(sentence.words[0].pos, "形容詞")
+    eq_(sentence.words[1].surface, "空")
+    eq_(sentence.words[1].pos, "名詞")
+
+
+def test_mecab_case2():
+    sentence = mecab.parse("金縛りにあったように動けない")
+    eq_(sentence.words[0].surface, "金縛り")
+    eq_(sentence.words[0].pos, "名詞")
+    eq_(sentence.words[1].surface, "に")
+    eq_(sentence.words[1].pos, "助詞")
+    eq_(sentence.words[2].surface, "あっ")
+    eq_(sentence.words[2].base, "ある")
+    eq_(sentence.words[2].pos, "動詞")
+    eq_(sentence.words[3].surface, "た")
+    eq_(sentence.words[3].pos, "助動詞")
+    eq_(sentence.words[4].surface, "よう")
+    eq_(sentence.words[4].pos, "名詞")
+    eq_(sentence.words[5].surface, "に")
+    eq_(sentence.words[5].pos, "助詞")
+    eq_(sentence.words[6].surface, "動け")
+    eq_(sentence.words[6].pos, "動詞")
+    eq_(sentence.words[6].base, "動ける")
+    eq_(sentence.words[7].surface, "ない")
+    eq_(sentence.words[7].pos, "助動詞")
