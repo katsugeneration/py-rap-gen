65a5bd3a659bec2e3a08ca7c5f1c0dd098b2d267
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2019-05-26 17:19:08 +0900

    Add shortest path search function

--- py_rap_gen/graph.py
+++ py_rap_gen/graph.py
@@ -1,6 +1,7 @@
 # Limitations under the MIT License.
 # Copyright 2019 Katsuya Shimabukuro.
 """Graph Shortes Path Solution."""
+import sys
 
 
 class Node(object):
@@ -8,14 +9,14 @@ class Node(object):
     def __init__(self, start_pos, word):
         self.start_pos = start_pos
         self.word = word
-        self.score = 0
+        self.cost = 0
         self.prev = None
 
     def __eq__(self, other):
         return (
             self.start_pos == other.start_pos and
             self.word == other.word and
-            self.score == other.score and
+            self.cost == other.cost and
             self.prev == other.prev
         )
 
@@ -23,18 +24,16 @@ class Node(object):
         return hash((
             self.start_pos,
             self.word,
-            self.score,
+            self.cost,
             self.prev))
-
-
-BOS = Node(0, "")
-EOS = Node(-1, "")
 
 
 class Graph(object):
     """Graph Object."""
     def __init__(self):
         self.nodes = []  # common endo position words list
+        self.BOS = Node(0, "")
+        self.EOS = Node(-1, "")
 
     @classmethod
     def construct_graph(cls, prefix_searcher, tone_list, tone):
@@ -52,11 +51,66 @@ class Graph(object):
 
         for _ in range(len(tone) + 2):
             g.nodes.append([])
-        g.nodes[0] = [BOS]
-        g.nodes[len(tone) + 1] = [EOS]
+        g.nodes[0] = [g.BOS]
+        g.EOS = Node(len(tone), "")
+        g.nodes[len(tone) + 1] = [g.EOS]
 
         for i in range(len(tone)):
             for s in prefix_searcher.prefix_search(tone[i:]):
                 g.nodes[i + len(s)].extend([Node(i, w) for w in tone_list[s]])
 
         return g
+
+    def get_node_cost(self, node):
+        """Return node cost.
+
+        Args:
+            node (Node): target node object.
+
+        Return:
+            cost (Int): node cost value.
+        """
+        # TODO: return predifiend cost.
+        return 0
+
+    def get_edge_cost(self, prev, node):
+        """Return prev to node edge cost.
+
+        Args:
+            prev (Node): previous node object.
+            node (Node): target node object.
+
+        Return:
+            cost (Int): edge cost value.
+        """
+        # TODO: return predifiend cost.
+        return 0
+
+    def search_shortest_path(self):
+        """Return graph shortest path.
+
+        Return:
+            path (List[Node]): node list constructs shortest path.
+        """
+        N = len(self.nodes)
+
+        # cost calculation (viterbi algorithm)
+        for i in range(1, N):
+            for node in self.nodes[i]:
+                node.cost = sys.maxsize
+                node_cost = self.get_node_cost(node)
+                for prev in self.nodes[node.start_pos]:
+                    edge_cost = self.get_edge_cost(prev, node)
+                    cost = prev.cost + edge_cost + node_cost
+                    if cost < node.cost:
+                        node.cost = cost
+                        node.prev = prev
+
+        # search shortest path
+        node = self.EOS
+        result = []
+        while node != self.BOS:
+            result.insert(0, node)
+            node = node.prev
+
+        return result
--- tests/test_graph.py
+++ tests/test_graph.py
@@ -20,8 +20,14 @@ prefix_searcher = common_prefix_search.DoubleArray(tone_list.keys())
 
 def test_construct_graph():
     g = graph.Graph.construct_graph(prefix_searcher, tone_list, 'aoioa')
-    eq_([graph.BOS], g.nodes[0])
+    eq_([g.BOS], g.nodes[0])
     eq_(set([graph.Node(0, 'か'), graph.Node(0, 'あ'), graph.Node(0, 'さ')]), set(g.nodes[1]))
     eq_(set([graph.Node(0, 'あお'), graph.Node(0, 'かお'), graph.Node(0, 'さと'), graph.Node(1, 'と')]),
         set(g.nodes[2]))
-    eq_([graph.EOS], g.nodes[6])
+    eq_([g.EOS], g.nodes[6])
+
+def test_search_shortest_path():
+    g = graph.Graph.construct_graph(prefix_searcher, tone_list, 'aoioa')
+    path = g.search_shortest_path()
+    eq_('あおい', path[0].word)
+    eq_('もか', path[1].word)
