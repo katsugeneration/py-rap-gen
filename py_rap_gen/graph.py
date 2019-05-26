7b5209896b6fbdc8dd393b6402422ce18beb3998
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2019-05-26 16:19:55 +0900

    Add latice graph constructor

---
+++ py_rap_gen/graph.py
@@ -0,0 +1,63 @@
+# Limitations under the MIT License.
+# Copyright 2019 Katsuya Shimabukuro.
+"""Graph Shortes Path Solution."""
+
+
+class Node(object):
+    """Graph Node Object."""
+    def __init__(self, start_pos, word):
+        self.start_pos = start_pos
+        self.word = word
+        self.score = 0
+        self.prev = None
+
+    def __eq__(self, other):
+        return (
+            self.start_pos == other.start_pos and
+            self.word == other.word and
+            self.score == other.score and
+            self.prev == other.prev
+        )
+
+    def __hash__(self):
+        return hash((
+            self.start_pos,
+            self.word,
+            self.score,
+            self.prev))
+
+
+BOS = Node(0, "")
+EOS = Node(-1, "")
+
+
+class Graph(object):
+    """Graph Object."""
+    def __init__(self):
+        self.nodes = []  # common endo position words list
+
+    @classmethod
+    def construct_graph(cls, prefix_searcher, tone_list, tone):
+        """Construct convert graph.
+
+        Args:
+            prefix_searcher (TrieBase): trie data
+            tone_list (Hash[String, List[String]]): tone to tone dictionary.
+            tone (String): target tone.
+
+        Return:
+            graph (Graph): new graph object.
+        """
+        g = Graph()
+
+        for _ in range(len(tone) + 1):
+            g.nodes.append([])
+        g.nodes[0] = [BOS]
+        g.nodes[len(tone)] = [EOS]
+
+        for i in range(len(tone)):
+            for j in range(i, len(tone)):
+                for s in prefix_searcher.search(tone[i:j], max_len=(j-i)):
+                    g.nodes[j].extend([Node(i, w) for w in tone_list[s]])
+
+        return g
---
+++ tests/test_graph.py
@@ -0,0 +1,27 @@
+from nose.tools import ok_, eq_
+from py_rap_gen import graph
+from py_rap_gen import common_prefix_search
+import pickle
+
+    
+tone_list = {
+    'a': ['あ', 'か', 'さ'],
+    'ao': ['あお', 'かお', 'さと'],
+    'aoi': ['あおい', 'さとみ'],
+    'o': ['と'],
+    'oi': ['とい', 'こい', 'とし'],
+    'oio': ['たいよ', 'はいりょ'],
+    'i': ['き', 'し'],
+    'io': ['みこ', 'しお'],
+    'oa': ['もか', 'もさ'],
+}
+prefix_searcher = common_prefix_search.DoubleArray(tone_list.keys())
+
+
+def test_construct_graph():
+    g = graph.Graph.construct_graph(prefix_searcher, tone_list, 'aoioa')
+    eq_([graph.BOS], g.nodes[0])
+    eq_(set([graph.Node(0, 'か'), graph.Node(0, 'あ'), graph.Node(0, 'さ')]), set(g.nodes[1]))
+    eq_(set([graph.Node(0, 'あお'), graph.Node(0, 'かお'), graph.Node(0, 'さと'), graph.Node(1, 'と')]),
+        set(g.nodes[2]))
+    eq_([graph.EOS], g.nodes[5])
