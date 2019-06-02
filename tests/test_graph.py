40441c61c6b072483ff3a00bf68fc0325dc8ea34
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2019-06-02 10:29:08 +0900

    Add iterator test

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -59,3 +59,31 @@ def test_structured_perceptron_multiple_sentence():
     eq_('と', path[1].word)
     eq_('と', path[2].word)
     eq_('さ', path[3].word)
+
+def test_structured_perceptron_iterator():
+    class iteratorWrapper():
+        def __init__(self, func):
+            self._func = func
+
+        def __iter__(self):
+            return self._func()
+
+    def data():
+        for s, d in [('aoioa',['あお', 'し', 'もさ']), ('aooa', ['あ', 'と', 'と', 'さ'])]:
+            yield (s, d)
+
+    learner = graph.StructuredPerceptron()
+    learner.train(iteratorWrapper(data), prefix_searcher, tone_list)
+    g = graph.Graph.construct_graph(prefix_searcher, tone_list, 'aoioa')
+    g.learner = learner
+    path = g.search_shortest_path()
+    eq_('あお', path[0].word)
+    eq_('し', path[1].word)
+    eq_('もさ', path[2].word)
+    g = graph.Graph.construct_graph(prefix_searcher, tone_list, 'aooa')
+    g.learner = learner
+    path = g.search_shortest_path()
+    eq_('あ', path[0].word)
+    eq_('と', path[1].word)
+    eq_('と', path[2].word)
+    eq_('さ', path[3].word)
