1c8e289db2848bfcd8af49bf5a4591584904d9f7
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2019-06-09 18:10:21 +0900

    Add demo code

---
+++ demo/server.py
@@ -0,0 +1,30 @@
+# Limitations under the MIT License.
+# Copyright 2019 Katsuya Shimabukuro.
+import responder
+import pickle
+import pathlib
+from py_rap_gen import generator, graph
+
+
+# Load pre-trained objects
+with open('mecab_tone_yomi.pkl', 'rb') as w:
+    tone_list = pickle.load(w)
+with open('prefix_searcher.pkl', 'rb') as w:
+    prefix_searcher = pickle.load(w)
+with open('learner.pkl', 'rb') as w:
+    learner = pickle.load(w)
+
+route = pathlib.Path(__file__).parent
+api = responder.API(static_dir=route.joinpath('static'))
+api.add_route("/", static=True)
+
+
+@api.route("/generate")
+def greet_world(req, resp):
+    # return generating rap
+    sentence = req.params['sentence']
+    resp.media = {"result": generator.generate_rapv2(sentence, tone_list, prefix_searcher, learner)}
+
+
+def main():
+    api.run()
