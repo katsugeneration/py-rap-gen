ea7a127272e918aa4a1f5cbca5846234fdb0501a
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2019-07-06 10:18:42 +0900

    Update preprocessor to support pos-tag info

--- py_rap_gen/preprocess.py
+++ py_rap_gen/preprocess.py
@@ -16,6 +16,7 @@ from py_rap_gen import mecab
 TONE_PATH = 'mecab_tone_yomi.pkl'
 PREFIX_SEARCHER_PATH = 'prefix_searcher.pkl'
 COUNTER_2GRAM_PATH = 'counter_2gram.pkl'
+WORD2POS_PATH = 'word2pos.pkl'
 LEARNER_PATH = 'learner.pkl'
 DATA_PATH = 'data'
 
@@ -73,11 +74,11 @@ def _process_syntax(line):
             ret_pronounce = ""
             ret_pos = ""
             continue
-        elif '名詞' not in ret_pos and word.pos not in ['接続詞', '形容詞', '動詞', '名詞', '連体詞', '副詞']:
-            ret_kana += word.surface
-            ret_pronounce += word.pronounce
-            if ret_pos == '':
-                ret_pos = word.pos
+        # elif '名詞' not in ret_pos and word.pos not in ['接続詞', '形容詞', '動詞', '名詞', '連体詞', '副詞']:
+        #     ret_kana += word.surface
+        #     ret_pronounce += word.pronounce
+        #     if ret_pos == '':
+        #         ret_pos = word.pos
         else:
             result.append(ret_kana + " " + ret_pronounce + " " + ret_pos)
             ret_kana = word.surface
@@ -85,7 +86,7 @@ def _process_syntax(line):
             if word.pos == '名詞':
                 ret_pos = word.pos + '-' + word.pos1
             elif word.pos == '助詞':
-                ret_pos = word.pos + '-' + word.surface
+                ret_pos = word.pos + '-' + word.pos1
             elif word.pos == '動詞':
                 ret_pos = word.pos + '-' + word.pos1
             else:
@@ -114,7 +115,6 @@ def _mix_tone_and_kana(tones, kanas):
         ret.append(tuple(kanas))
     else:
         ret.append(tuple(tones[:-1] + [kanas[-1]]))
-        ret.append(tuple(tones[:-2] + kanas[-2:]))
     if len(tones) >= 4:
         ret.append(tuple(tones))
 
@@ -136,27 +136,25 @@ def _create_tone_list():
                 words = filter(lambda w: w.strip() != '', words)
                 yield from words
 
-    def train_data_2gram(lcounter):
+    def train_data_2gram():
         g = graph.Graph()
         with open(DATA_PATH, 'r') as f:
             for line in f:
                 line = line.strip()
                 words = line.split('\t')
-                words = filter(lambda w: w.strip() != '', words)
-                words = [w if w in lcounter._items else graph.UNKNOW_WORD for w in words]
-                words = [g.BOS.word] + words + [g.EOS.word]
-                yield from [words[i].split()[0] + '_' + words[i+1].split()[0]
-                            for i in range(len(words) - 1)
-                            if words[i] not in graph.UNKNOW_WORD and words[i+1] not in graph.UNKNOW_WORD]
+                words = list(filter(lambda w: w.strip() != '', words))
+                words = [' '.join([g.BOS.word]*3)] + words + [' '.join([g.EOS.word]*3)]
+                yield from [words[i].split()[2] + '_' + words[i+1].split()[2] for i in range(len(words) - 1)]
 
     lcounter = counter.LossyCounter(epsilon=1e-7)
     lcounter.count(train_data())
     lcounter_2gram = counter.LossyCounter(epsilon=1e-7)
-    lcounter_2gram.count(train_data_2gram(lcounter))
+    lcounter_2gram.count(train_data_2gram())
     print(len(lcounter._items))
     print(len(lcounter_2gram._items))
 
     tone_list = {}
+    word2pos = {}
     count = 0
     for w in lcounter._items:
         chars = w.split()[1]
@@ -169,16 +167,19 @@ def _create_tone_list():
             count += 1
             continue
 
-        for t in _mix_tone_and_kana(tones, kana):
-            if t not in tone_list:
-                tone_list[t] = []
-            tone_list[t].append(w.split()[0])
+        word = w.split()[0]
+        if word not in word2pos:
+            word2pos[word] = w.split()[2]
+            for t in _mix_tone_and_kana(tones, kana):
+                if t not in tone_list:
+                    tone_list[t] = []
+                tone_list[t].append(word)
     print("Remove Count:", count)
     print('Total Count:', sum(1 for t in tone_list for l in tone_list[t]))
-    return tone_list, lcounter_2gram
+    return tone_list, lcounter_2gram, word2pos
 
 
-def _train_graph(prefix_searcher, tone_list, lcounter_2gram):
+def _train_graph(prefix_searcher, tone_list, lcounter_2gram, word2pos):
     """Training Structured learner.
 
     Return:
@@ -195,6 +196,8 @@ def _train_graph(prefix_searcher, tone_list, lcounter_2gram):
         count = 0
         with open(DATA_PATH, 'r') as f:
             for line in f:
+                if random.random() > 0.01:
+                    continue
                 count += 1
                 line = line.strip()
                 words = list(filter(lambda w: w.strip() != '', line.split('\t')))
@@ -217,8 +220,9 @@ def _train_graph(prefix_searcher, tone_list, lcounter_2gram):
     learner = graph.StructuredPerceptron()
     learner.N = 1e7
     learner.epochs = 1
+    learner.default_cost = 100
     learner.construct_feature(list(lcounter_2gram._items))
-    learner.train(iteratorWrapper(train_data), prefix_searcher, tone_list)
+    learner.train(iteratorWrapper(train_data), prefix_searcher, tone_list, word2pos)
     return learner
 
 
@@ -230,14 +234,16 @@ def main():
         with open('articles.txt', 'r') as f:
             for line in f:
                 w.write(_process_syntax(line) + '\n')
-    tone_list, lcounter_2gram = _create_tone_list()
+    tone_list, lcounter_2gram, word2pos = _create_tone_list()
     with open(TONE_PATH, 'wb') as w:
         pickle.dump(tone_list, w, pickle.HIGHEST_PROTOCOL)
     with open(COUNTER_2GRAM_PATH, 'wb') as w:
         pickle.dump(lcounter_2gram, w, pickle.HIGHEST_PROTOCOL)
+    with open(WORD2POS_PATH, 'wb') as w:
+        pickle.dump(word2pos, w, pickle.HIGHEST_PROTOCOL)
     prefix_searcher = trie.DoubleArray(tone_list.keys())
     with open(PREFIX_SEARCHER_PATH, 'wb') as w:
         pickle.dump(prefix_searcher, w, pickle.HIGHEST_PROTOCOL)
-    learner = _train_graph(prefix_searcher, tone_list, lcounter_2gram)
+    learner = _train_graph(prefix_searcher, tone_list, lcounter_2gram, word2pos)
     with open(LEARNER_PATH, 'wb') as w:
         pickle.dump(learner, w, pickle.HIGHEST_PROTOCOL)
